846856fa759d981e47cbbf76cb146f6efda7ed3e
Dominik Fuchß <develop@fuchss.org>
2022-11-25 12:44:41 +0100

    Cleanup Methods

--- src/main/kotlin/org/fuchss/matrix/mensa/Main.kt
+++ src/main/kotlin/org/fuchss/matrix/mensa/Main.kt
@@ -6,7 +6,6 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import net.folivo.trixnity.client.MatrixClient
 import net.folivo.trixnity.client.getRoomId
-import net.folivo.trixnity.client.getSender
 import net.folivo.trixnity.client.room.message.text
 import net.folivo.trixnity.client.store.repository.createInMemoryRepositoriesModule
 import net.folivo.trixnity.clientserverapi.model.authentication.IdentifierType
@@ -31,7 +30,6 @@ fun main() {
             scope = scope
         ).getOrThrow()
 
-
         val matrixBot = MatrixBot(matrixClient, config)
         matrixBot.subscribeAllEvents { event -> println(event) }
         matrixBot.subscribe { event -> handleTextMessage(event, matrixBot) }
@@ -47,31 +45,38 @@ private suspend fun handleTextMessage(event: Event<RoomMessageEventContent.TextM
     }
 
     if (message.startsWith("!name")) {
-        val newNameInRoom = message.substring("!name".length).trim()
-        if (newNameInRoom.isNotBlank()) matrixBot.renameInRoom(event.getRoomId()!!, newNameInRoom)
+        changeUsername(event, matrixBot, message)
     }
 
-
     if (message.startsWith("!mensa")) {
-        val food = MensaAPI().foodAtDate()
-        var response = ""
-        if (food.isEmpty() || food.all { mensa -> mensa.mensaLinesAtDate()!!.isEmpty() }) response = "Kein Essen heute :("
-        else {
-            for (m in food) {
-                response += "# ${m.name}\n"
-                for (l in m.mensaLinesAtDate()!!) {
-                    response += "## ${l.title}\n"
-                    for (meal in l.meals) response += "* ${meal.name}\n"
-                }
+        printMensa(event, matrixBot)
+    }
+}
+
+private suspend fun changeUsername(event: Event<RoomMessageEventContent.TextMessageEventContent>, matrixBot: MatrixBot, message: String) {
+    val newNameInRoom = message.substring("!name".length).trim()
+    if (newNameInRoom.isNotBlank()) matrixBot.renameInRoom(event.getRoomId()!!, newNameInRoom)
+}
+
+private suspend fun printMensa(event: Event<RoomMessageEventContent.TextMessageEventContent>, matrixBot: MatrixBot) {
+    val food = MensaAPI().foodAtDate()
+    var response = ""
+    if (food.isEmpty() || food.all { mensa -> mensa.mensaLinesAtDate()!!.isEmpty() }) response = "Kein Essen heute :("
+    else {
+        for (m in food) {
+            response += "# ${m.name}\n"
+            for (l in m.mensaLinesAtDate()!!) {
+                response += "## ${l.title}\n"
+                for (meal in l.meals) response += "* ${meal.name}\n"
             }
         }
+    }
 
-        val document = Parser.builder().build().parse(response)
-        val html = HtmlRenderer.builder().build().render(document)
+    val document = Parser.builder().build().parse(response)
+    val html = HtmlRenderer.builder().build().render(document)
 
-        matrixBot.room().sendMessage(event.getRoomId()!!) {
-            text(response, format = "org.matrix.custom.html", formattedBody = html)
-        }
+    matrixBot.room().sendMessage(event.getRoomId()!!) {
+        text(response, format = "org.matrix.custom.html", formattedBody = html)
     }
 }
 
