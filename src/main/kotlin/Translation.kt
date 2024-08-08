892de10be9737f8bd9732866574b6131508ccb01
Dominik Fuchß <develop@fuchss.org>
2024-08-08 18:10:56 +0200

    Set temparature to 0

--- src/main/kotlin/Translation.kt
+++ src/main/kotlin/Translation.kt
@@ -31,7 +31,7 @@ class TranslationService(private val config: TranslationConfig?) {
     }
 
     private fun createChatModel(config: TranslationConfig): ChatLanguageModel {
-        val ollama = OllamaChatModel.builder().baseUrl(config.ollamaServerUrl).modelName(config.model).timeout(Duration.ofMinutes(5))
+        val ollama = OllamaChatModel.builder().baseUrl(config.ollamaServerUrl).modelName(config.model).timeout(Duration.ofMinutes(5)).temperature(0.0)
         if (config.ollamaUser != null && config.ollamaPassword != null) {
             ollama.customHeaders(mapOf("Authorization" to Credentials.basic(config.ollamaUser, config.ollamaPassword)))
         }
